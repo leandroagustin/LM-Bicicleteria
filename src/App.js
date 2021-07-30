6028daa381ffb11a44c4dcc7fd9a00cd3e81946d
leandroagustinmena <leandromena_94@hotmail.com>
2021-07-29 21:46:56 -0300

    cambios en index y en function app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Primer desafio del curso de React JS.
         </p>
         <a
           className="App-link"
